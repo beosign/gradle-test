19285266c0662e7a80650d555d0715453f0322a8
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-08-21 08:29:41 +0200

    Added JUnit, Log4j2, Speed4j and main and test class

--- src/main/java/de/beosign/test/gradle/Main.java
+++ src/main/java/de/beosign/test/gradle/Main.java
@@ -1,7 +1,27 @@
 package de.beosign.test.gradle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ecyrd.speed4j.StopWatch;
+import com.ecyrd.speed4j.StopWatchFactory;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Main");
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static StopWatchFactory stopWatchFactory = StopWatchFactory.getInstance("loggingFactory");
+
+    public static void main(String[] args) throws InterruptedException {
+        LOGGER.info("Start");
+
+        StopWatch sw = stopWatchFactory.getStopWatch("main");
+        sw.start();
+
+        Thread.sleep(1000);
+        sw.stop();
+
+    }
+
+    public static StopWatchFactory getStopWatchFactory() {
+        return stopWatchFactory;
     }
 }
